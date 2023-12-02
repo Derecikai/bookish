5d927fe8cf28b7c856ec8e9e18ef139d9f0da4d7
rainman226 <dota2romaniarobi@gmail.com>
2023-12-02 17:56:21 +0200

    created some classes

---
+++ backend/src/main/java/uvt/tw/bookish/entities/Book.java
@@ -0,0 +1,32 @@
+package uvt.tw.bookish.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String author;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genre genreID;
+
+    @Column(length = 20)
+    private String ISBN;
+
+    private String description;
+}
