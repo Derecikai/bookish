28d1e891cf819d51253faf4cabde369f7ff0a779
rainman226 <dota2romaniarobi@gmail.com>
2024-01-18 08:16:20 +0200

    added ai connection to frontend

--- frontend/bookish/src/Bookshelf/BookDetailsModal.jsx
+++ frontend/bookish/src/Bookshelf/BookDetailsModal.jsx
@@ -1,5 +1,5 @@
 // BookDetailsModal.js
-import React from 'react';
+import React, { useState } from 'react';
 import './BookDetailsModal.css';
 import axios from 'axios';
 
@@ -7,13 +7,17 @@ import axios from 'axios';
 const BookDetailsModal = ({ book, onClose }) => {
 
 
+
+  const [data,setData] = useState(null) 
+
   const getRec = async () =>{
     try{
       //pune api-ul tau aici cu book.title sau book.id sau dupa ce iti trebe sa cauti
       //foloseste `` nu "" sau '' ca sa poti sa scri gen get${book.title} in endpoint
       //dupa vezi ce iti scrie in c ${book.title}
-      const response = await axios.get(`localhost:8080/ai/generate?message=${book.title}`)
+      const response = await axios.get(`http://localhost:8080/ai/generate?message=${book.title}`)
       console.log(response)
+      setData(response.data.generation)
     }catch(err)
     {
       console.log(err);
@@ -34,6 +38,7 @@ const BookDetailsModal = ({ book, onClose }) => {
         {/* Add more book details as needed */}
         <button onClick={onClose}>Close</button>
         <button onClick={getRec}>Get recomandation</button>
+        <p>{data && data}</p>
       </div>
     </div>
   );
